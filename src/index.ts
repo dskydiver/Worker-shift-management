1cd79833adf9d22f33c36a0ee5fd25417b2e635d
dskydiver <dastanmukhan6@gmail.com>
2024-07-22 20:17:24 +0300

    update api

--- src/index.ts
+++ src/index.ts
@@ -2,33 +2,38 @@ import prisma from '../database'
 import logger from '../logger'
 import { build } from './server'
 
-const start = async () => {
-  const server = await build()
+// const start = async () => {
+//   const server = await build()
 
-  // Graceful Shutdown
-  const gracefulShutdown = async (signal: string) => {
-    logger.info(`* Received signal: ${signal} *`)
-    logger.info('Closing fastify server...')
-    await server.close()
+//   // Graceful Shutdown
+//   const gracefulShutdown = async (signal: string) => {
+//     logger.info(`* Received signal: ${signal} *`)
+//     logger.info('Closing fastify server...')
+//     await server.close()
 
-    // Here you'd also handle your database disconnection, if needed.
-    await prisma.$disconnect()
+//     // Here you'd also handle your database disconnection, if needed.
+//     await prisma.$disconnect()
 
-    logger.info('Server successfully closed')
-    process.exit(0)
-  }
+//     logger.info('Server successfully closed')
+//     process.exit(0)
+//   }
 
-  // Handling termination and interrupt signals
-  process.on('SIGTERM', () => gracefulShutdown('SIGTERM'))
-  process.on('SIGINT', () => gracefulShutdown('SIGINT'))
+//   // Handling termination and interrupt signals
+//   process.on('SIGTERM', () => gracefulShutdown('SIGTERM'))
+//   process.on('SIGINT', () => gracefulShutdown('SIGINT'))
 
-  const portDetail: number = Number(process.env.PORT) || 5000
+//   const portDetail: number = Number(process.env.PORT) || 5000
 
-  await server.listen({ port: portDetail })
-  logger.info(`server running on port ${portDetail}`)
+//   await server.listen({ port: portDetail })
+//   logger.info(`server running on port ${portDetail}`)
+// }
+
+// start().catch((error) => {
+//   logger.error(error)
+//   process.exit(1)
+// })
+export default async function handler(request: any, reply: any) {
+  const app = await build()
+  app.ready()
+  app.server.emit('request', request, reply)
 }
-
-start().catch((error) => {
-  logger.error(error)
-  process.exit(1)
-})
